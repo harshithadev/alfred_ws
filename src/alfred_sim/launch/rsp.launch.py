fbd42e773e2528edef217f1416e06812a9da3bea
harshithadev <harshithadevineni16@gmail.com>
2025-03-04 19:57:27 +0530

    visualize robot on gazebo, use_sim_time config parameter added

--- src/alfred_sim/launch/rsp.launch.py
+++ src/alfred_sim/launch/rsp.launch.py
@@ -7,13 +7,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('alfred_sim'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = Command(['xacro ', xacro_file,])
+    robot_description_config = Command(['xacro ', xacro_file, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config,}
+    params = {'robot_description': robot_description_config,'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -28,6 +31,10 @@ def generate_launch_description():
 
     # Launch!
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
         node_robot_state_publisher,
         node_joint_state_publisher,
     ])
